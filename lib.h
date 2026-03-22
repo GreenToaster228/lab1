0a2647fb168db5a0166f7e7ada2e8351512739b3
YAKRUT-real <yakrut-real@yandex.ru>
2026-03-22 17:53:30 +0300

    fixing lost media

--- lib.h
+++ lib.h
@@ -2,36 +2,36 @@
 #define pi 3.141592653f
 typedef struct
 {
-	int a;
-	int b;
-	int c;
+    int a;
+    int b;
+    int c;
 } Triangle;
 
 char check(const Triangle *triangle)
 {
-	if (triangle -> a <= 0 || triangle -> b <= 0 || triangle -> c <= 0) return 0;
-	return ((triangle -> a + triangle -> b > triangle -> c) &&  (triangle -> a + triangle -> c > triangle -> b) && (triangle -> c + triangle -> b > triangle -> a));
+    if (triangle -> a <= 0 || triangle -> b <= 0 || triangle -> c <= 0) return 0;
+    return ((triangle -> a + triangle -> b > triangle -> c) &&  (triangle -> a + triangle -> c > triangle -> b) && (triangle -> c + triangle -> b > triangle -> a));
 }
 
 int perimeter(const Triangle *triangle)
 {
-	if (check(triangle) == 0) return 0;
-	return triangle -> a + triangle -> b + triangle ->c;
+    if (check(triangle) == 0) return 0;
+    return triangle -> a + triangle -> b + triangle ->c;
 }
 
 int area(const Triangle *triangle)
 {
-	if (check(triangle) == 0) return 0;
-	int p = perimeter(triangle)/2;
-	return sqrt(p * (p - triangle -> a) * (p - triangle -> b) * (p - triangle -> c));
+    if (check(triangle) == 0) return 0;
+    int p = perimeter(triangle)/2;
+    return sqrt(p * (p - triangle -> a) * (p - triangle -> b) * (p - triangle -> c));
 }
 
 char isosceles(const Triangle *triangle)
 {
-	if (check(triangle) == 0) return 0;
-	return (triangle -> a == triangle -> b || triangle -> a == triangle -> c || triangle -> b == triangle -> c);
+    if (check(triangle) == 0) return 0;
+    return (triangle -> a == triangle -> b || triangle -> a == triangle -> c || triangle -> b == triangle -> c);
 }
-//divider
+
 struct circle
 {
     float length;
@@ -43,13 +43,13 @@ float rad_sector_area(struct circle c, float radian_angle)
 {
     if(radian_angle<0)
         radian angle = -radian_angle
-    return radian_angle*c.radius*c.radius/(2.0f);
+        return radian_angle*c.radius*c.radius/(2.0f);
 }
 float deg_sector_area(struct circle c, float degree_angle)
 {
     if(degree_angle<0)
         degree_angle = -degree_angle
-    return degree_angle*c.radius*c.radius*pi/360.0f;
+        return degree_angle*c.radius*c.radius*pi/360.0f;
 }
 
 struct circle new_circle(float r)
