9a9dfa4ae31f8d9419a167e5c9a949dce9b65edb
Evg <122084187+Ardz1@users.noreply.github.com>
2026-03-23 11:34:00 +0700

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,27 +1,5 @@
 #include <math.h>
 #include <stdio.h>
-typedef struct
-{
-	const int a;
-	const int b;
-	const int c;
-} Triangle;
-
-int perimeterTriangle(const Triangle *triangle)
-{
-	return triangle -> a + triangle -> b + triangle ->c;
-}
-
-int areaTriangle(const Triangle *triangle)
-{
-	int p = perimeterTriangle(triangle)/2;
-	return sqrt(p * (p - triangle -> a) * (p - triangle -> b) * (p - triangle -> c));
-}
-
-char isosceles(const Triangle *triangle)
-{
-	return (triangle -> a == triangle -> b || triangle -> a == triangle -> c || triangle -> b == triangle -> c);
-}
 
 typedef struct
 {
