fc2870919733b7fe5667803ea0f856716b1b9e65
Evg <122084187+Ardz1@users.noreply.github.com>
2026-03-23 11:19:32 +0700

    Update test.c

--- test.c
+++ test.c
@@ -5,8 +5,8 @@ int main()
 {
     Rectangle testR = {0, 0};
     printf("Введите стороны прямоугольника в формате: 1 2: ");
-    scanf("%d %d %d", &(test.a), &(test.b));
+    scanf("%d %d", &(testR.a), &(testR.b));
     printf("Периметр: %d\n", perimeterRectangle(&testR));
-    printf("Площадь: %d\n", areaRectangel(&testR));
-    printf("Диагональ: %d", diagonal(&testR));
+    printf("Площадь: %d\n", areaRectangle(&testR));
+    printf("Диагональ: %f", diagonal(&testR));
 }
